a0ca97c9b524734099c161e904da4524bc063d27
maris <mari.seppanen4@metropolia.fi>
2020-09-01 09:19:17 +0300

    socket

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+"use strict";
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.static("public"));
+//run client connection
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected", socket.id);
+  });
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log("joinRoom:", room);
+  });
+  socket.on("username", (username) => {
+    console.log("username:", username);
+    io.emit("username", username);
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: ", msg);
+    io.emit("chat message", msg);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+http.listen(3000, () => {
+  console.log("listening on port 3000");
 });
